28906d915a6ada44901cde3aa26381b54219496e
Macbook Air <macbookair@David-Ramirez.local>
2016-08-14 12:40:49 -0700

    add a watchCreateTaskInput function and pass the createTaskInput login inside this new function

--- src/factories/todo-factory.js
+++ src/factories/todo-factory.js
@@ -18,10 +18,24 @@ const todoFactory = angular.module('app.todoFactory', [])
 		_.remove($scope.todos, todo => todo.task === todoToDelete.task)	
 	}
 
+	function watchCreateTaskInput(params,$scope, val){
+		const createHasInput = params.createHasInput
+		if (!val && createHasInput){
+			$scope.todos.pop();
+			params.createHasInput = false;
+		}else if (val && !createHasInput) {
+			$scope.todos.push({ task: val, isCompleted: false });
+			params.createHasInput = true;
+		} else if (val && createHasInput) {
+			$scope.todos[$scope.todos.length - 1].task = val;
+		}
+	}
+
 	return{
 		createTask,
 		updateTask,
-		deleteTask
+		deleteTask,
+		watchCreateTaskInput
 	};
 });
 
