46cb7737fc88e875228fa9ab3684cbf02b5231f8
Macbook Air <macbookair@David-Ramirez.local>
2016-08-13 18:44:42 -0700

    add two todo task to todos db

--- src/todos/todos.js
+++ src/todos/todos.js
@@ -1,3 +1,12 @@
-export default function() {
-	
+export default function($scope) {
+	$scope.todos=[
+	{
+		task: 'do dishes',
+		isCompleted: false
+	},
+	{
+		task: 'Walk the dog',
+		isCompleted: true
+	}
+	];
 }
